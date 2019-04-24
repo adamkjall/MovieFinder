63a0638ef0cfbd8a83bf7babd33164d64311c2ba
Adam <bottarn.botvidsson@gmail.com>
2019-04-24 12:34:18 +0200

    created a service file that handles tmdb api calls

--- src/modules/movie-browser/movie-browser.container.js
+++ src/modules/movie-browser/movie-browser.container.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { AppBar } from 'material-ui';
+import { getTopMovies, searchMovies, getMovieDetails } from './movie-browser.services'
 
 class MovieBrowser extends React.Component {
   render() {
+    
     return (
       <div>
         <AppBar title='Movie Browser' />
         <Container>
           <Row>
             <p>Search will go here</p>
+            
           </Row>
           <Row>
             <p>Movie list will go here</p>

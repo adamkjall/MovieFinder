5ff83c9efa7e0f3b8dbae1c9436e3464e00641e0
Adam <bottarn.botvidsson@gmail.com>
2019-04-24 11:02:55 +0200

    added a store to the project

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import movieBrowserReducer from './modules/movie-browser/movie-browser.reducer';
+
+// The root reducer will serve as the parent for all the other reducers.
+// Add module reducers to the root level here
+const rootReducer = combinerReducers({
+  movieBrowser: movieBrowserReducer
+});
+
+// Logs information about dispatched actions to the console
+const loggerMiddleware = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
+
+export default store;

fe48b3e71ee9f97c42c1136644c7c58479e25021
Adam <bottarn.botvidsson@gmail.com>
2019-04-24 10:48:20 +0200

    added a helper function that create reducers

---
+++ src/modules/common/redux.helpers.js
@@ -0,0 +1,9 @@
+// Helper function that enables passing an object
+// with the action.type as the key and the reducer
+// function as the value
+export const createReducer = (initialState = {}, actionHandlerKeyFuncs = {}) => {
+  return (state = initialState, action) => {
+    const actionHandler = actionHandlerKeyFuncs[action.type];
+    return actionHandler ? actionHandler(state, action) : state;
+  }
+};
